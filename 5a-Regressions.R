74adab71eab23dfe032307463fafa72b2eb1ca78
mmann1123 <mmann1123@login3.cm.cluster>
2017-05-01 11:01:34 -0400

    showing selections

--- 5a-Regressions.R
+++ 5a-Regressions.R
@@ -125,6 +125,44 @@ attr(vswheat2$terms,'term.labels')[vswheat2$varselect.interp],
 attr(vswheat3$terms,'term.labels')[vswheat3$varselect.interp])
 
 
+ [1] "Y_COORD"                    "X_COORD"
+ [3] "ZONECODE"                   "WHEATAREA"
+ [5] "REGIONCODE"                 "Year"
+ [7] "WHEATNIMSEED"               "WHEATFERT_CHEMICAL_AMT"
+ [9] "WHEATSEED2AREA"             "WHEATNIMSEED_P"
+[11] "WHEATFERT_CHEMICAL_AREA"    "WHEATDAMAGEAREA_P"
+[13] "WHEATFERT_CHEMICAL_AMT_P"   "WHEATSERRAREA"
+[15] "WHEATDAMAGEAREA"            "WHEATMERR4AREA"
+[17] "WHEATFERT_CHEMICAL_AREA_P"  "WHEATDAMAGE_WEATHER_AREA_P"
+[19] "WHEATMERR1AREA"             "WHEATEXTAREA"
+[21] "WHEATDAMAGE_WEATHER_AREA"   "WHEATFERT_NATURAL_AREA"
+[23] "WHEATIMSEED"                "WHEATFERT_NATURAL_AREA_P"
+[25] "WHEATSEED1AREA"             "WHEATIMSEED_P"
+[27] "WHEATSERRAREA_P"            "WHEATSEED2AREA_P"
+[29] "WHEATSEED1AREA_P"           "WHEATMERR4AREA_P"
+[31] "WHEATDAMAGE_PESTS_AREA"     "WHEATEXTAREA_P"
+[33] "WHEATMERR1AREA_P"           "WHEATDAMAGE_DROUGHT_AREA_P"
+[35] "WHEATMERR5AREA"             "WHEATDAMAGE_DROUGHT_AREA"
+[37] "WHEATMERR5AREA_P"           "G_AUC_Qnt"
+[39] "G_mx"                       "A_max"
+[41] "A_Qnt"                      "A_max_Qnt"
+[43] "A_mn"                       "G_AUC"
+[45] "G_AUC2"                     "G_Qnt"
+[47] "A_AUC"                      "G_mn"
+[49] "G_mx_Qnt"                   "T_G_Qnt"
+[51] "G_min"                      "A_AUC_Qnt"
+[53] "ETA_G_AUC_diff_mn"          "PET_G_mn"
+[55] "PET_G_sd"                   "PET_A_max"
+[57] "PET_A_sd"                   "PET_G_AUC_trailing"
+[59] "ETA_G_AUC_diff_90th"        "ETA_G_mx"
+[61] "ETA_G_Qnt"                  "PET_A_AUC"
+[63] "ETA_G_sd"                   "PET_A_mn"
+[65] "ETA_A_sd"                   "ETA_G_AUC2"
+
+
+
+
+
 # get names of selected variables at interpretation phase of variables selection
 
 c(attr(vswheat0$terms,'term.labels')[vswheat0$varselect.pred],
@@ -133,6 +171,21 @@ attr(vswheat2$terms,'term.labels')[vswheat2$varselect.pred],
 attr(vswheat3$terms,'term.labels')[vswheat3$varselect.pred])
 
 
+ [1] "WHEATNIMSEED"               "WHEATFERT_CHEMICAL_AMT"
+ [3] "WHEATSEED2AREA"             "WHEATNIMSEED_P"
+ [5] "WHEATFERT_CHEMICAL_AREA"    "WHEATDAMAGEAREA_P"
+ [7] "WHEATFERT_CHEMICAL_AMT_P"   "WHEATSERRAREA"
+ [9] "WHEATEXTAREA"               "WHEATFERT_NATURAL_AREA"
+[11] "WHEATIMSEED"                "WHEATMERR4AREA_P"
+[13] "WHEATDAMAGE_PESTS_AREA"     "WHEATEXTAREA_P"
+[15] "WHEATMERR1AREA_P"           "WHEATDAMAGE_DROUGHT_AREA_P"
+[17] "WHEATMERR5AREA_P"           "G_AUC_Qnt"
+[19] "A_max_Qnt"                  "A_mn"
+[21] "G_AUC"                      "G_mx_Qnt"
+[23] "T_G_Qnt"                    "A_AUC_Qnt"
+[25] "ETA_G_AUC_diff_mn"          "PET_G_mn"
+[27] "PET_G_sd"                   "PET_A_max"
+[29] "PET_A_sd"
 
 
 
